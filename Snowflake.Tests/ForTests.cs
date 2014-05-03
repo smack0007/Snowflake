46b451d0dcf33df93517ca0f97a1c90a10d41ad5
smack0007 <zachary.snow@gmail.com>
2014-05-03 16:59:43 +0800

    Added basic for and while tests.

---
+++ Snowflake.Tests/ForTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Snowflake.Tests
+{
+	[TestFixture]
+	public class ForTests : LanguageTestFixture
+	{
+		[Test]
+		public void For_With_False_Eval_Expr_Body_Never_Executed()
+		{
+			AssertScriptReturnValue(0, @"
+var y = 0;
+
+for (var x = 0; false; x += 1) {
+	y += 1;
+}
+
+return y;
+");
+		}
+
+		[Test]
+		public void For_Exits_Correctly()
+		{
+			AssertScriptReturnValue(10, @"
+var y = 0;
+
+for (var x = 0; x != 10; x += 1) {
+	y += 1;
+}
+
+return y;
+");
+		}
+	}
+}
--- Snowflake.Tests/WhileTests.cs
+++ Snowflake.Tests/WhileTests.cs
@@ -9,5 +9,32 @@ namespace Snowflake.Tests
     [TestFixture]
     public class WhileTests : LanguageTestFixture
     {
+		[Test]
+		public void While_False_Body_Is_Never_Executed()
+		{
+			AssertScriptReturnValue(0, @"
+var x = 0;
+
+while (false) {
+	x += 1;
+}
+
+return x;
+");
+		}
+
+		[Test]
+		public void While_Exits_Correctly()
+		{
+			AssertScriptReturnValue(10, @"
+var x = 0;
+
+while (x != 10) {
+	x += 1;
+}
+
+return x;
+");
+		}
     }
 }
